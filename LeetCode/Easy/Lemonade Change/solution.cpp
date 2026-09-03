ae6a9af51b5b17a274e5fa255608bb6f997f427f
Anand karthik <anand2007lins@gmail.com>
2026-09-03 15:36:34 +0530

    Solved: Lemonade Change [Easy]

---
+++ LeetCode/Easy/Lemonade Change/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        if(bills[0]!=5) return false;
+        int fives=0,tens=0;
+        for(int i=0;i<bills.size();i++)
+        {
+            if(bills[i]==5){
+                fives++;
+                continue;
+            }
+            else if(bills[i]==10){
+                if(fives>0) {
+                    fives--;   
+                }
+                else return false; 
+                tens++;
+            }
+            else
+            {
+                if(fives>0 && tens>0)
+                {
+                    tens--; fives--;
+                } 
+                else if(fives>2)
+                {
+                    fives-=3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+};
